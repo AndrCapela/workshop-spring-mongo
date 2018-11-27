f90c06c8192fb82bdd0433e62fcf213aaf2148d7
André Capela <andr.capela@gmail.com>
2018-11-27 11:47:36 -0200

    Primeiro commit - projeto criado

---
+++ src/main/java/com/andrcapela/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,12 @@
+package com.andrcapela.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+}
