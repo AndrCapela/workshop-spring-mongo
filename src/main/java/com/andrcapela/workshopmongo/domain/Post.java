b55b2b15e334023c472c72347ea92e43a8fca5e0
André Capela <andr.capela@gmail.com>
2018-12-03 10:37:17 -0200

    Acrescentando comentarios aos POSTS

--- src/main/java/com/andrcapela/workshopmongo/domain/Post.java
+++ src/main/java/com/andrcapela/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.andrcapela.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.andrcapela.workshopmongo.dto.AuthorDTO;
+import com.andrcapela.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -47,6 +51,14 @@ public class Post implements Serializable {
 
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	public String getTitle() {
@@ -97,5 +109,6 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
+
 }
 
